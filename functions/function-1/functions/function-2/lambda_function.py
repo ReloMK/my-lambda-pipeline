3da9d9811544bffade37c3464b886710065c3eaf
ReloMK <125446802+ReloMK@users.noreply.github.com>
2026-03-19 12:25:22 +0200

    Add lambda_handler function to return JSON response

--- functions/function-1/functions/function-2/lambda_function.py
+++ functions/function-1/functions/function-2/lambda_function.py
@@ -1,1 +1,11 @@
+import json
+from datetime import datetime
 
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Hello from Function 2!",
+            "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        })
+    }

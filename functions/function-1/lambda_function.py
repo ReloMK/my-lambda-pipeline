908cdeb8b330cf09b43eb8d747ace862fa727e1c
ReloMK <125446802+ReloMK@users.noreply.github.com>
2026-03-19 13:51:27 +0200

    Add headers to lambda function response

--- functions/function-1/lambda_function.py
+++ functions/function-1/lambda_function.py
@@ -3,6 +3,9 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
         "body": json.dumps({
             "message": "Hello from Function 1!",
             "function": "function-1"

53761f2239b319f63dd628449cbd0497d91da0f9
ReloMK <125446802+ReloMK@users.noreply.github.com>
2026-03-19 13:36:29 +0200

    Add headers to lambda response

--- functions/function-2/lambda_function.py
+++ functions/function-2/lambda_function.py
@@ -4,6 +4,9 @@ from datetime import datetime
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
         "body": json.dumps({
             "message": "Hello from Function 2 - updated!",
             "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")

fbde08776c0f1c9f32a3d98b28539973a723a0a9
ReloMK <125446802+ReloMK@users.noreply.github.com>
2026-03-19 11:01:01 +0200

    Fix print statement in test_lambda.py

--- test_lambda.py
+++ test_lambda.py
@@ -6,6 +6,6 @@ def test_handler():
     body = json.loads(response["body"])
     assert response["statusCode"] == 200
     assert body["message"] == "Hello from my CI/CD pipeline!"
-    print("✅ Test passed!")
+    print("Test passed!")
 
 test_handler()

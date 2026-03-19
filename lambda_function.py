e30c40373617ba08e784d292651fc5489f624bdb
ReloMK <125446802+ReloMK@users.noreply.github.com>
2026-03-19 11:14:27 +0200

    Fix formatting issue in lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello from my CI/CD pipeline - version 2!",
+           "message": "Hello from my CI/CD pipeline - version 2!",
             "version": "2.0.0"
         })
     }
